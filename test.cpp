d08b5a5d5821bced92a73e264a1208de81093961
eggeek <zsz921130@gmail.com>
2014-07-19 18:07:39 +0800

    commit

---
+++ test.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.cpp
+ *        Created:  2014年07月19日 18时07分07秒
+ *       Compiler:  gcc
+ *
+ * =====================================================================================
+ */
+#include <queue>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+typedef long long llong;
+#define maxn 1
+
+
+
+
