91bab0034b3d4175ffdd7120203a6dd46224e8d4
eggeek <zsz921130@gmail.com>
2016-01-29 16:44:34 +0800

    srm680 450 constructive

---
+++ SRM 680/BearSpans.cpp
@@ -0,0 +1,239 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+typedef long long llong;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+template<class T> inline T min(T a, T b, T c) {return min(min(a,b),c);}
+template<class T> inline T min(T a, T b, T c, T d) {return min(min(a,b),min(c,d));}
+template<class T> inline T max(T a, T b, T c) {return max(max(a,b),c);}
+template<class T> inline T max(T a, T b, T c, T d) {return max(max(a,b),max(c,d));}
+const int INF = 1e9;
+const llong INF_LL = 4e18;
+const double pi = acos(-1.0);
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*----------------------------------------*/
+#define maxn 1010
+
+class BearSpans {
+    public:
+    int dgr[maxn], w[maxn][maxn], cnte;
+    vector<int> s[maxn];
+    queue<int> q[15];
+
+    void link(int u, int v) {
+        dgr[u]++;
+        dgr[v]++;
+        w[v][u] = w[u][v] = ++cnte;
+    }
+
+    void extraEdge(int p, int extra) {
+        int cnt=0;
+        for (auto i: s[p]) {
+            if (i == p || w[i][p]) continue;
+            link(p, i);
+            cnt++;
+            if (cnt == extra) break;
+        }
+    }
+
+    vector<int> findAnyGraph(int n, int m, int k) {
+        vector<int> impossible {-1};
+        if (k > 12) return impossible;
+        if ((1<<k) > n) return impossible;
+
+        memset(dgr, 0, sizeof(dgr));
+        memset(w, 0, sizeof(w));
+        cnte = 0;
+        int l = n - (1<<k) + 1;
+        for (int i=1; i<l; i++) {
+             w[i][i+1] = w[i+1][i] = ++cnte;
+        }
+        for (int i=l; i<=n; i++) {
+            q[0].push(i);
+            s[0].clear();
+            s[i].push_back(i);
+        }
+
+        for (int i=0; i<k; i++) {
+            while (!q[i].empty()) {
+                int a = q[i].front(); q[i].pop();
+                int b = q[i].front(); q[i].pop();
+                link(a, b);
+                if (i+1-dgr[a]) extraEdge(a, i+1-dgr[a]);
+                if (i+1-dgr[b]) extraEdge(b, i+1-dgr[b]);
+                // merge
+                for (auto x: s[a]) s[b].push_back(x);
+                q[i+1].push(b);
+            }
+            if (cnte > m) return impossible;
+        }
+        for (int i=1; i<=n && cnte < m; i++) {
+            for (int j=i+1; j<=n && cnte < m; j++) if (!w[i][j]) link(i, j);
+        }
+        vector<int> ans(2*m);
+        for (int i=1; i<=n; i++) {
+            for (int j=i+1; j<=n; j++) if (w[i][j]) {
+                ans[2*w[i][j] - 2] = i;
+                ans[2*w[i][j] - 1] = j;
+            }
+        }
+        return ans;
+    }
+};
+
+// CUT begin
+ifstream data("BearSpans.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(int n, int m, int k, vector<int> __expected) {
+    time_t startClock = clock();
+    BearSpans *instance = new BearSpans();
+    vector<int> __result = instance->findAnyGraph(n, m, k);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int n;
+        from_stream(n);
+        int m;
+        from_stream(m);
+        int k;
+        from_stream(k);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(n, m, k, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1454051737;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 450 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "BearSpans (450 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
