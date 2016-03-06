91a14ee25713a9dd421d419a48660ff73517fca2
eggeek <zsz921130@gmail.com>
2016-03-06 11:14:56 +1100

    finish srm682 250 & srm683 250

---
+++ SRM 682/SmilesTheFriendshipUnicorn.cpp
@@ -0,0 +1,190 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+typedef long long llong;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+template<class T> inline T min(T a, T b, T c) {return min(min(a,b),c);}
+template<class T> inline T min(T a, T b, T c, T d) {return min(min(a,b),min(c,d));}
+template<class T> inline T max(T a, T b, T c) {return max(max(a,b),c);}
+template<class T> inline T max(T a, T b, T c, T d) {return max(max(a,b),max(c,d));}
+const int INF = 1e9;
+const llong INF_LL = 4e18;
+const double pi = acos(-1.0);
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*----------------------------------------*/
+vi e[2002];
+bool vis[2002];
+
+bool dfs(int cur, int l) {
+    if (l == 5) return true;
+    vis[cur] = true;
+    bool ans = false;
+    for (auto i: e[cur]) {
+        if (!vis[i]) {
+            ans = dfs(i, l+1);
+            if (ans) return ans;
+        }
+    }
+    vis[cur] = false;
+    return ans;
+}
+
+class SmilesTheFriendshipUnicorn {
+    public:
+    string hasFriendshipChain(int N, vector<int> A, vector<int> B) {
+        for (int i=0; i<SZ(A); i++) {
+            e[A[i]].push_back(B[i]);
+            e[B[i]].push_back(A[i]);
+        }
+        for (int i=0; i<N; i++) {
+            if (dfs(i, 1)) return "Yay!";
+        }
+        return ":(";
+    }
+};
+
+// CUT begin
+ifstream data("SmilesTheFriendshipUnicorn.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int N, vector<int> A, vector<int> B, string __expected) {
+    time_t startClock = clock();
+    SmilesTheFriendshipUnicorn *instance = new SmilesTheFriendshipUnicorn();
+    string __result = instance->hasFriendshipChain(N, A, B);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int N;
+        from_stream(N);
+        vector<int> A;
+        from_stream(A);
+        vector<int> B;
+        from_stream(B);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, A, B, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1456201810;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 300 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "SmilesTheFriendshipUnicorn (300 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
