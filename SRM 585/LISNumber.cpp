1a86b9d2c8c078433a46baff0d507647f9a6377a
eggeek <zsz921130@gmail.com>
2016-03-30 14:17:34 +1100

    pass srm 585 250, 500

---
+++ SRM 585/LISNumber.cpp
@@ -0,0 +1,199 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr << #x << " = " << x << endl;
+#else
+#define dbg(x)
+#endif
+typedef long long llong;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+template<class T> inline T min(T a, T b, T c) {return min(min(a,b),c);}
+template<class T> inline T min(T a, T b, T c, T d) {return min(min(a,b),min(c,d));}
+template<class T> inline T max(T a, T b, T c) {return max(max(a,b),c);}
+template<class T> inline T max(T a, T b, T c, T d) {return max(max(a,b),max(c,d));}
+const int INF = 1e9;
+const llong INF_LL = 4e18;
+const double pi = acos(-1.0);
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*----------------------------------------*/
+#define N 1400
+const llong mod = 1000000007L;
+llong c[N][N], f[40][N];
+
+void initc() {
+    c[0][0] = 1;
+    for (int i=1; i<N; i++) {
+        c[i][0] = c[i][i] = 1;
+        for (int j=1; j<i; j++) c[i][j] = (c[i-1][j-1] + c[i-1][j]) % mod;
+    }
+}
+
+class LISNumber {
+    public:
+    int count(vector<int> cnt, int K) {
+        initc();
+        memset(f, 0, sizeof(f));
+        int n = SZ(cnt);
+        f[0][cnt[0]] = 1;
+        for (int sum=0, i=0; i<n-1; i++) {
+            sum += cnt[i];
+            for (int j=cnt[i]; j<=K; j++) if (f[i][j]) {
+                for (int add=0; add <= cnt[i+1] && j+add <= K; add++) {
+                    int rest = cnt[i+1] - add;
+                    int pos = sum - (j - 1) + rest;
+                    llong inc;
+                    inc = f[i][j] * c[pos+add-1][pos-1] % mod * c[j][rest] % mod;
+                    f[i+1][j+add] += inc;
+                    f[i+1][j+add] %= mod;
+                }
+            }
+            //for (int j=cnt[i+1]; j<=K; j++) printf("f[%d][%d]=%lld\n", i+1, j, f[i+1][j]);
+        }
+        return f[n-1][K];
+    }
+};
+
+// CUT begin
+ifstream data("LISNumber.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> cardsnum, int K, int __expected) {
+    time_t startClock = clock();
+    LISNumber *instance = new LISNumber();
+    int __result = instance->count(cardsnum, K);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> cardsnum;
+        from_stream(cardsnum);
+        int K;
+        from_stream(K);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(cardsnum, K, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1459297933;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "LISNumber (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
