59783c9e3b4920dbc7c8c54c2bba93372e4cbdd4
eggeek <zsz921130@gmail.com>
2016-03-30 14:20:14 +1100

    pass srm 686 300

---
+++ SRM 686/BracketSequenceDiv1.cpp
@@ -0,0 +1,218 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr << #x << " = " << x << endl;
+#else
+#define dbg(x)
+#endif
+typedef long long llong;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+template<class T> inline T min(T a, T b, T c) {return min(min(a,b),c);}
+template<class T> inline T min(T a, T b, T c, T d) {return min(min(a,b),min(c,d));}
+template<class T> inline T max(T a, T b, T c) {return max(max(a,b),c);}
+template<class T> inline T max(T a, T b, T c, T d) {return max(max(a,b),max(c,d));}
+const int INF = 1e9;
+const llong INF_LL = 4e18;
+const double pi = acos(-1.0);
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*----------------------------------------*/
+#define N 50
+int ok[N][N];
+llong f[N][N];
+int stk[N], top;
+string ss;
+
+bool match(string s, int l, int r) {
+    if (l > r) return true;
+    if (s[l] == '(' && s[r] == ')') return true;
+    if (s[l] == '[' && s[r] == ']') return true;
+    return false;
+}
+
+bool check(string s, int l, int r) {
+    top = 0;
+    for (int i=l; i<=r; i++) {
+        if (s[i] == '(') stk[++top] = i;
+        else if (s[i] == '[') stk[++top] = i;
+        else {
+            if (s[i] == ']') {
+                if (!top) return false;
+                else if (s[stk[top]] == '[') top--;
+                else return false;
+            } else if (s[i] == ')') {
+                if (!top) return false;
+                else if (s[stk[top]] == '(') top--;
+                else return false;
+            }
+        }
+    }
+    return top == 0;
+}
+
+void initok(string s) {
+    int l = SZ(s);
+    for (int i=0; i<l; i++) {
+        for (int j=i+1; j<l; j++) ok[i][j] = check(s, i, j);
+    }
+}
+
+llong dfs(int l, int r) {
+    if (l > r) return 1;
+    if (f[l][r] != -1) return f[l][r];
+    if (l == r) return f[l][r]=1; //empty
+
+    llong ans = 0;
+    ans += dfs(l+1, r); // delete s[l]
+    for (int i=l+1; i<=r; i++) if (match(ss, l, i)) { // not delete s[l]
+        llong t1 = dfs(l+1, i-1);
+        llong t2 = dfs(i+1, r);
+        ans += t1 * t2;
+    }
+    f[l][r] = ans;
+    //dbg(l); dbg(r); dbg(f[l][r]); dbg(ok[l][r]);
+    return f[l][r];
+}
+
+class BracketSequenceDiv1 {
+    public:
+    long long count(string s) {
+        initok(s);
+        int l = SZ(s);
+        ss = s;
+        memset(f, -1, sizeof(f));
+        llong ans = dfs(0, l-1) - 1;
+        return ans;
+    }
+};
+
+// CUT begin
+ifstream data("BracketSequenceDiv1.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string s, long long __expected) {
+    time_t startClock = clock();
+    BracketSequenceDiv1 *instance = new BracketSequenceDiv1();
+    long long __result = instance->count(s);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string s;
+        from_stream(s);
+        next_line();
+        long long __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(s, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1459213208;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 300 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "BracketSequenceDiv1 (300 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
