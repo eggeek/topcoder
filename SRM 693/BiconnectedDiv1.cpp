a6eebf16a3c86bc62b41eac2b4b50b07c5b7c611
eggeek <zsz921130@gmail.com>
2016-07-04 21:26:25 +1000

    pass 693 250

---
+++ SRM 693/BiconnectedDiv1.cpp
@@ -0,0 +1,198 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define x first
+#define y second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr << #x << " = " << x << endl;
+#else
+#define dbg(x)
+#endif
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+template<class T> inline T min(T a, T b, T c) {return min(min(a,b),c);}
+template<class T> inline T min(T a, T b, T c, T d) {return min(min(a,b),min(c,d));}
+template<class T> inline T max(T a, T b, T c) {return max(max(a,b),c);}
+template<class T> inline T max(T a, T b, T c, T d) {return max(max(a,b),max(c,d));}
+const int INF = 1e9;
+const ll INF_LL = 4e18;
+const double pi = acos(-1.0);
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*----------------------------------------*/
+#define N 101
+int dp[N][3][3], n;
+
+class BiconnectedDiv1 {
+    public:
+
+    int dfs(int i, int cur, int nxt, vi& w1, vi& w2) {
+      if (dp[i][cur][nxt] != -1) return dp[i][cur][nxt];
+      if (i == n-2) {
+        if (nxt < 2) return dp[i][cur][nxt] = INF;
+        else return dp[i][cur][nxt] = w1[i];
+      }
+      int res, tmp;
+      res = dfs(i+1, nxt, 2, w1, w2) + w1[i] + w2[i];
+      if (cur) {
+        if (nxt -1) {
+          tmp= dfs(i+1, nxt-1, 2, w1, w2) + w2[i];
+          res = min(res, tmp);
+        }
+        if (nxt == 2) {
+          tmp = dfs(i+1, nxt, 1, w1, w2) + w1[i];
+          res = min(res, tmp);
+        }
+      }
+
+      return dp[i][cur][nxt] = res;
+    }
+
+    int minimize(vector<int> w1, vector<int> w2) {
+        n = SZ(w1) + 1;
+        memset(dp, -1, sizeof(dp));
+        int ans = dfs(0, 0, 1, w1, w2);
+        return ans;
+    }
+};
+
+// CUT begin
+ifstream data("BiconnectedDiv1.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> w1, vector<int> w2, int __expected) {
+    time_t startClock = clock();
+    BiconnectedDiv1 *instance = new BiconnectedDiv1();
+    int __result = instance->minimize(w1, w2);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> w1;
+        from_stream(w1);
+        vector<int> w2;
+        from_stream(w2);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(w1, w2, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1467627454;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "BiconnectedDiv1 (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
