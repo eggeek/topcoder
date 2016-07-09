46a6706aa0891f868a38c079e97060f9efe307da
eggeek <zsz921130@gmail.com>
2016-07-09 18:49:17 +1000

    pass 593 div1 450

---
+++ SRM 593/MayTheBestPetWin.cpp
@@ -0,0 +1,214 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define x first
+#define y second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr << #x << " = " << x << endl;
+#else
+#define dbg(x)
+#endif
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+template<class T> inline T min(T a, T b, T c) {return min(min(a,b),c);}
+template<class T> inline T min(T a, T b, T c, T d) {return min(min(a,b),min(c,d));}
+template<class T> inline T max(T a, T b, T c) {return max(max(a,b),c);}
+template<class T> inline T max(T a, T b, T c, T d) {return max(max(a,b),max(c,d));}
+const int INF = 1e9;
+const ll INF_LL = 4e18;
+const double pi = acos(-1.0);
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*----------------------------------------*/
+const int N = 50 * 20010;
+bool dp[2][N];
+int suma, sumb;
+
+class MayTheBestPetWin {
+    public:
+
+    bool check(int d, vi& A, vi& B) {
+      int n = SZ(A);
+      int l = sumb - d;
+      int r = suma + d;
+      for (int i=0; i<2; i++)
+        for (int j=0; j<=r; j++) dp[i][j] = false;
+      dp[0][0] = true;
+      int cur = 0;
+      for (int i=0; i<n; i++) {
+        int nxt = 1 - cur;
+        for (int j=0; j<=r; j++) if (dp[cur][j]) {
+          if (j + A[i] + B[i] <= r)
+            dp[nxt][j + A[i] + B[i]] = true;
+          dp[nxt][j] = true;
+        }
+        cur = nxt;
+      }
+      for (int i=l; i<=r; i++) if (dp[cur][i]) return true;
+      return false;
+    }
+
+    int bsearch(int l, int r, vi& A, vi& B) {
+      int best = r;
+      while (l <= r) {
+        int mid = (l + r) >> 1;
+        if (check(mid, A, B)) {
+          best = mid;
+          r = mid - 1;
+        } else l = mid + 1;
+      }
+      return best;
+    }
+
+    int calc(vector<int> A, vector<int> B) {
+        suma = 0, sumb = 0;
+        for (int i=0; i<SZ(A); i++) {
+          suma += A[i], sumb += B[i];
+        }
+        int l = (sumb - suma) / 2;
+        int r = sumb;
+        int ans = bsearch(l, r, A, B);
+        return ans;
+    }
+};
+
+// CUT begin
+ifstream data("MayTheBestPetWin.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> A, vector<int> B, int __expected) {
+    time_t startClock = clock();
+    MayTheBestPetWin *instance = new MayTheBestPetWin();
+    int __result = instance->calc(A, B);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> A;
+        from_stream(A);
+        vector<int> B;
+        from_stream(B);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(A, B, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1468045678;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 450 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "MayTheBestPetWin (450 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
