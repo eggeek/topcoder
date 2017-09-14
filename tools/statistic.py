8daa5eaef6210465a8fa7e5f1a1186754d96aaea
eggeek <eggeek.me@gmail.com>
2017-09-14 23:00:01 +1000

    modify: sort by difficulty

--- tools/statistic.py
+++ tools/statistic.py
@@ -2,6 +2,9 @@
 # -*-: encoding: utf-8 -*-
 from tinydb import TinyDB
 
+def difficulty(item):
+    return item['submissions'] * item['correct_rate'] * item['average_pts']
+
 
 def main():
     db = TinyDB("db.json")
@@ -12,14 +15,17 @@ def main():
         levels[row['level']].append(row)
 
     out = []
-    header = '|%-35s|%-40s|%-6s|' % ("round", "problem", "solved")
+    header = '|%-35s|%-40s|%-6s|%-10s|%5s|%10s|' % ("round", "problem", "solved", "ave_pts",
+            "rate", "submission")
     for k, v in levels.items():
         out.append('# LEVEL %d' % k)
         out.append('-' * len(header))
         out.append(header)
         out.append('-' * len(header))
+        v.sort(key=lambda x: difficulty(x), reverse=True)
         for i in v:
-            out.append('|%-35s|%-40s|%-6s|' % (i['round'], i['name'], i['solved']))
+            out.append('|%-35s|%-40s|%-6s|%-10.2lf|%-4.3lf|%10s|' % (i['round'], i['name'], i['solved'],
+                i['average_pts'], i['correct_rate'], i['submissions']))
         out.append('*' * len(header))
     with open("../statistic.txt", 'w') as f:
         for i in out:
